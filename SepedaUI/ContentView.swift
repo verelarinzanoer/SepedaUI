d6f8cfc51c76fb570c45aa2b3fd9a8bbc39b74d2
verelarinzanoer <ahmadverelzanoe@gmail.com>
2021-02-03 11:22:34 +0700

    Menambahkan product

--- SepedaUI/ContentView.swift
+++ SepedaUI/ContentView.swift
@@ -7,10 +7,31 @@
 
 import SwiftUI
 
+//model data
+struct ProductModel: Identifiable {
+    let id: Int
+    let namaProduk: String
+    let fotoProduk: String
+    let hargaProduk: Int
+    let lokasi: String
+    let ratingCount: Int
+    let jumlahRating: Int
+    
+    init(id: Int,  namaProduk: String, fotoProduk: String, hargaProduk: Int, lokasi: String, ratingCount: Int, jumlahRating: Int) {
+        
+        self.id = id
+        self.namaProduk = namaProduk
+        self.fotoProduk = fotoProduk
+        self.hargaProduk = hargaProduk
+        self.lokasi = lokasi
+        self.ratingCount = ratingCount
+        self.jumlahRating = jumlahRating
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+       Product()
     }
 }
 
@@ -19,3 +40,81 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+struct Product: View {
+    var body: some View{
+        VStack(alignment: .leading){
+            // fto
+            ZStack(alignment:  .topTrailing){
+                Image("foto1")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(height: 250)
+                    .clipped()
+                
+                Button(action: {print("Liked")}){
+                    Image(systemName: "heart")
+                        .padding()
+                        .foregroundColor(.red)
+                }
+        }
+            
+            Text("Sepeda Polygon")
+                .font(.title)
+                .bold()
+                .padding(.leading)
+                .padding(.trailing)
+            
+            Text("Rp. 2.000.000")
+                .font(.title)
+                .bold()
+                .foregroundColor(.red)
+                .padding(.leading)
+                .padding(.trailing)
+            
+            HStack{
+                Image(systemName: "mappin.circle")
+                Text("kab. Banyumas")
+                
+            }
+            .padding(.leading)
+            .padding(.trailing)
+            
+            HStack{
+                HStack{
+                    Image(systemName: "star.fill")
+                        .foregroundColor(.yellow)
+                    Image(systemName: "star.fill")
+                        .foregroundColor(.yellow)
+                    Image(systemName: "star.fill")
+                        .foregroundColor(.yellow)
+                }
+            }
+            .padding(.leading)
+            .padding(.trailing)
+            .padding(.top, 5)
+            
+            Button(action: {print("ditambahkan")}){
+                HStack{
+                    Spacer()
+                    HStack{
+                        Image(systemName: "cart")
+                        Text("Tambah ke keranjang")
+                            .font(.callout)
+                            .padding()
+                    }
+                    Spacer()
+                }
+            }
+            .background(Color.green)
+            .foregroundColor(Color.white)
+            .cornerRadius(10)
+            .padding()
+     }
+        .background(Color("warna"))
+        .cornerRadius(15)
+        
+    }
+}
+
+
